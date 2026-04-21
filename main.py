86dd8b2408efecdc632b674c0c10e67a0d111242
sasiis80-gif <sasini.somaratne80@gmail.com>
2026-04-21 19:38:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+for x in range (10): #iterate the loop
+    if x % 20 == 0 : #condition 1
+       print("twist") 
+
+    elif x  % 15 == 0 : #condition 2
+        pass             #pass statement
+
+    elif x % 5 == 0 : #condition 3
+        print("fuzzz")    #display result
+
+    elif x % 3 == 0 : #condition 4
+        print("buzzz")    #display result
+
+    else:             #condition 5
+        print(x)          #display result    
